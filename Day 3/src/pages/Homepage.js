01892b8bffa8fd291aa8fb343c864231c31ae4d3
Kavyaa1706 <110534815+Kavyaa1706@users.noreply.github.com>
2023-09-15 14:57:09 +0530

    Add files via upload

---
+++ Day 3/src/pages/Homepage.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../assets/Homepage.css";
+import img from "../assets/company.jpg"
+import { Link } from "react-router-dom";
+
+import Navbarr from "./Navbar";
+export default function Home(){
+    return(
+<div className="niv" >
+    <div className="nav">
+        <Navbarr/>
+    </div>
+   <div>
+    <img className="image" src={img}></img>
+   </div>
+   <div className="black">
+
+   </div>
+   <div>
+    <h1 className="event">
+        <i>"Life is an Event!"</i>
+    </h1>
+   </div>
+   <div>
+    <h1 className="event1">
+        <i>"Make it memorable..!"</i>
+    </h1>
+   </div>
+   <div>
+    <Link to='/pages/Register'><button className="cus">Customer</button></Link>
+   </div>
+   <div>
+    <Link to='/pages/Register'><button className="em">Event Manager</button></Link>
+   </div>
+</div>
+    )
+}

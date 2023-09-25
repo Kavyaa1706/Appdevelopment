811a577f689c49094b532a543e85577fe7e0b108
Kavyaa1706 <110534815+Kavyaa1706@users.noreply.github.com>
2023-09-25 15:28:58 +0530

    Add files via upload

---
+++ Day8/src/pages/Dashboard.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../redux/userSlice'
+import Navbarr from './Navbar'
+import {FaSmile} from "react-icons/fa"
+import "../assets/Dashboard.css";
+function Dashboard() {
+    const user=useSelector(selectUser)
+  if (user === null) {
+   
+    return <div>Loading...</div>;
+  }
+    return (
+        
+        <>
+
+        <div className='nav'><Navbarr/></div>
+            <Layout />
+            <div className='main-wrapper'>
+                 <div className='welcome'>
+                 <div className='fs'><FaSmile/></div > 
+                    <h1 className='wel'>
+                         Welcome <span>{user.username} </span> ! </h1>
+                </div>  
+                <div className='fog'>
+
+                </div>
+                <div>
+                    <h1 className='ce'>  Crystal Events</h1>
+                </div>
+                <div>
+                    <p  ><div className='quote'> Sitback while we create the</div> <div className='color'>experience</div><div className='life'> of a lifetime</div></p>
+                </div>
+                <div>                          
+                    <p><div className='guest'>"Be Guest"</div> <div className='events'>at your own Event..!</div></p>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Dashboard
